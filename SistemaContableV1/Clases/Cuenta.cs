db87ce56e74866d98d80ed2a29bf48bfa6820d2c
brunoSacchetti <bruno.sacchetti@alumnos.frm.utn.edu.ar>
2023-10-07 18:40:17 -0300

    clases agregadas

---
+++ SistemaContableV1/Clases/Cuenta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaContableV1.Clases
+{
+    internal class Cuenta
+    {
+        //public string _id { get; set; }
+        public string nombreCuenta { get; set; }
+        public int codigoCuenta { get; set; }
+
+        public Cuenta(string nombre, int codigo)
+        {
+            this.nombreCuenta = nombre;
+            this.codigoCuenta = codigo;
+        }
+    }
+}
---
+++ SistemaContableV1/Clases/Empresa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaContableV1.Clases
+{
+    internal class Empresa
+    {
+        private string nombre { get; set; }
+        private string telefono { get; set; }
+        private string ciudad { get; set; }
+        private string email { get; set; }
+
+
+    }
+}
